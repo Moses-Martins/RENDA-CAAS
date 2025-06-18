f19aefe9c11c5f66fc9d18bc87315a2a2b6762fa
Moses Martins <mmoses061@gmail.com>
2025-06-18 21:18:15 +0100

    Updated Main.go

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/v1/me", controllers.Me).Methods("GET")
-	r.HandleFunc("/v1/renda360/register", controllers.RegisterRenda360).Methods("POST")
-	r.HandleFunc("/v1/scale/register", controllers.RegisterScale).Methods("POST")
-	r.HandleFunc("/v1/horizon/register", controllers.RegisterHorizon).Methods("POST")
+	r.HandleFunc("/v1/register/renda360", controllers.RegisterRenda360).Methods("POST")
+	r.HandleFunc("/v1/register/scale", controllers.RegisterScale).Methods("POST")
+	r.HandleFunc("/v1/register/horizon", controllers.RegisterHorizon).Methods("POST")
 	r.HandleFunc("/v1/login", controllers.Login).Methods("POST")
 	r.HandleFunc("/v1/admin/update-privilege", controllers.UpdateUserPrivilege).Methods("PATCH")
 	r.Handle("/v1/dashboard/{product}", middleware.AdminOrUserForProduct(controllers.UserCollection)(http.HandlerFunc(controllers.ProductDashboard))).Methods("GET")
